c5397fd0dbda7bf6c20b2c5a5bd37c6b394e21c6
rharrisharrisacquire234 <Richard.harris@harrisacquire.com>
2026-03-10 15:34:46 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,11 +19,11 @@ GOOGLE_SHEET_NAME = os.getenv("GOOGLE_SHEET_NAME", "Sheet1")
 # CONFIG
 # -------------------------------------------------
 BUSINESS_QUERIES = [
-    "Fencing Contractor in",
-    "Fence Installation Service in",
-    "Commercial Fencing Service in",
-    "Security Fencing Contractor in",
-    "Industrial Fencing Contractor in",
+    "Garden Fencing Service in",
+    "Fence Repair Service in",
+    "Perimeter Fencing Contractor in",
+    "Steel Fencing Contractor in",
+    "Temporary Fencing Service in",
 ]
 
 MAX_SCROLLS = 50
